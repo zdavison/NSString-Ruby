1324b38cc9ce584999315fc7e735608a996d83c9
Zachary Davison <zac.developer@gmail.com>
2013-02-27 16:23:12 +0000

    Initial Commit

---
+++ NSString+Ruby/NSString_Ruby.m
@@ -0,0 +1,13 @@
+//
+//  NSString_Ruby.m
+//  NSString+Ruby
+//
+//  Created by Zachary Davison on 27/02/2013.
+//  Copyright (c) 2013 ZD. All rights reserved.
+//
+
+#import "NSString_Ruby.h"
+
+@implementation NSString_Ruby
+
+@end
---
+++ NSString+RubyTests/NSString_RubyTests.m
@@ -0,0 +1,32 @@
+//
+//  NSString_RubyTests.m
+//  NSString+RubyTests
+//
+//  Created by Zachary Davison on 27/02/2013.
+//  Copyright (c) 2013 ZD. All rights reserved.
+//
+
+#import "NSString_RubyTests.h"
+
+@implementation NSString_RubyTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in NSString+RubyTests");
+}
+
+@end
