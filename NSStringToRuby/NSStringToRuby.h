c05c89420ff866b9a0354c60e95de639825f84b0
Shannon Wu <inatu@icloud.com>
2016-02-23 15:14:43 +0800

    Add Carthage support

---
+++ NSStringToRuby/NSStringToRuby.h
@@ -0,0 +1,20 @@
+//
+//  NSStringToRuby.h
+//  NSStringToRuby
+//
+//  Created by Shannon Wu on 2/23/16.
+//  Copyright © 2016 ZD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NSStringToRuby.
+FOUNDATION_EXPORT double NSStringToRubyVersionNumber;
+
+//! Project version string for NSStringToRuby.
+FOUNDATION_EXPORT const unsigned char NSStringToRubyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NSStringToRuby/PublicHeader.h>
+
+#import <NSStringToRuby/NSString+Ruby.h>
+#import <NSStringToRuby/NSMutableString+Ruby.h>
